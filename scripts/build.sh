2ac9b71fa4d2429591f61a79da74768f35558150
Vasileios Pasialiokis <whiterocket@outlook.com>
2024-10-05 11:21:15 +0300

    further improvements on ci/cd

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,70 @@
+#!/bin/sh
+set -e
+
+name=redirector
+timestamp=$(date '+%s')
+
 echo 'note that you might want to run this with admin rights'
 
-echo 'cleaning up'
-rm -r build
+cleanup() {
+  echo 'cleaning up'
+  rm -r build
+}
 
+container() {
+  mkdir -p container
+
+  echo 'downloading alpine'
+  curl https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.3-x86_64.tar.gz > alpine.tar.gz
+
+  echo 'creating base container'
+  tar -xf alpine.tar.gz -C container
+  systemd-nspawn -D container apk add php83-fpm
+
+  echo 'copying source code and configuration'
+  mkdir -p container/app
+  cp ../src/* container/app
+  cp ../etc/php-fpm.conf container/etc/php83/php-fpm.conf
+
+  echo 'creating image'
+  mksquashfs container/ container.squashfs
+}
+
+package() {
+  echo 'creating debian package'
+  dirname="$name"_"$timestamp"_amd64
+  mkdir -p $dirname/DEBIAN $dirname/srv/$name/storage
+  cp ../etc/Caddyfile ../etc/entrypoint container.squashfs $dirname/srv/$name
+  chmod 755 $dirname/DEBIAN
+  cat << EOF > $dirname/DEBIAN/control
+Package: $name
+Version: $timestamp
+Architecture: amd64
+Maintainer: Vasileios Pasialiokis <vas@tsuku.ro>
+Description: url redirect service (link shortener)
+Depends: systemd, systemd-container, caddy, dash
+EOF
+
+  cat << EOF > $dirname/DEBIAN/preinst
+#!/bin/sh
+systemctl stop container@redirector
+systemctl stop caddy
+EOF
+  chmod +x $dirname/DEBIAN/preinst
+
+  cat << EOF > $dirname/DEBIAN/postinst
+systemctl start container@redirector
+systemctl start caddy
+EOF
+  chmod +x $dirname/DEBIAN/postinst
+
+  dpkg-deb --build --root-owner-group $dirname
+}
+
+cleanup
 mkdir -p build
-
-echo 'downloading alpine'
-curl https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.3-x86_64.tar.gz > build/alpine.tar.gz
-
-echo 'creating base container'
-mkdir -p build/redirector
-tar -xf build/alpine.tar.gz -C build/redirector
-systemd-nspawn -D build/redirector apk add php83-fpm
-
-echo 'copying source code and configuration'
-mkdir -p build/redirector/opt/redirector
-cp src/* build/redirector/opt/redirector
-cp etc/php-fpm.conf build/redirector/etc/php83/php-fpm.conf
-
-echo 'creating image'
-mksquashfs build/redirector/ build/redirector.squashfs
-
-echo 'done'
+cd build
+container
+package
+cd ..
+echo 'done!'
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
-
 user=root
 domain=sexualise.it
 auth=$user@$domain
+pkg=`basename build/*deb`
 
-echo 'uploading container'
-scp build/redirector.squashfs $auth:/var/lib/machines
+echo 'uploading package'
+scp build/$pkg $auth:/root/$pkg
 
-echo 'uploading systemd service'
-scp etc/redirector.service $auth:/etc/systemd/system
+echo 'installing'
+ssh $auth apt install /root/$pkg
 
-echo 'uploading caddyfile'
-scp etc/redirector.caddy $auth:/etc/caddy
+echo 'cleanup'
+ssh $auth rm -v /root/$pkg
 
-echo 'reloading systemd daemons'
-ssh $auth systemctl daemon-reload
-
-echo 'restarting container'
-ssh $auth systemctl restart redirector
-
-echo 'restarting caddy'
-ssh $auth systemctl restart caddy
+echo 'done!'
