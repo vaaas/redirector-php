5737f6bae7068ec1f448edf40c2ded9b5c6f95b6
Vasileios Pasialiokis <whiterocket@outlook.com>
2025-10-28 12:06:03 +0200

    read only container

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 self=/srv/redirector
-exec systemd-nspawn                   \
-    -D $self/container                \
-    -M redirector                     \
-    --bind $self/storage:/app/storage \
-    --chdir /                         \
+exec systemd-nspawn                    \
+    -D      $self/container            \
+    -M      redirector                 \
+    --bind  $self/storage:/app/storage \
+    --chdir /                          \
+    --read-only                        \
     /usr/sbin/php-fpm84 -R
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,25 +4,26 @@ set -e
 alpine=https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.2-x86_64.tar.gz
 php=php84
 
-mkdir      container
+mkdir    container
 
-echo       'Downloading alpine'
-curl       $alpine > alpine.tar.gz
-tar -xf    alpine.tar.gz -C container
+echo     'Downloading alpine'
+curl     $alpine > alpine.tar.gz
+tar      -xf alpine.tar.gz \
+         -C container
 
-echo       'Installing packages'
-cp         /etc/resolv.conf container/etc/resolv.conf
-chroot     container apk add $php-fpm
-rm         container/etc/resolv.conf
+echo     'Installing packages'
+cp       /etc/resolv.conf container/etc/resolv.conf
+chroot   container apk add $php-fpm
+rm       container/etc/resolv.conf
 
-echo       'Copying files'
-mkdir -p   container/app \
-           container/app/storage
-cp -r      app       \
-           index.php \
-           container/app
-cp         etc/php-fpm.conf \
-           container/etc/$php/php-fpm.conf
+echo     'Copying files'
+mkdir -p container/app \
+         container/app/storage
+cp -r    app       \
+         index.php \
+         container/app
+cp       etc/php-fpm.conf \
+         container/etc/$php/php-fpm.conf
 
-echo       'fixing permissions'
-chmod -R   o+r container
+echo     'fixing permissions'
+chmod -R o+r container
