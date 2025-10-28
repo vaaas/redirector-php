e8c7434970f9f223ade3b573ad2c8097d0b2d850
Vasileios Pasialiokis <whiterocket@outlook.com>
2025-10-28 11:38:51 +0200

    proceed gracefully when links are empty

--- app/Providers/LinksProvider.php
+++ app/Providers/LinksProvider.php
@@ -5,6 +5,7 @@ use DataAccess\IFileSystem;
 use DataAccess\Links;
 use DependencyInjection\Container;
 use DependencyInjection\IProvider;
+use Error;
 
 final class LinksProvider implements IProvider {
     private const LOCATION = "links";
@@ -16,8 +17,14 @@ final class LinksProvider implements IProvider {
 
     private function get(Container $container): Links {
         $fs = $container->get(IFileSystem::class);
+        $contents = "";
+        try {
+            $contents = $fs->get(self::LOCATION) ?: "";
+        } catch (Error $e) {
+            error_log("Failed to read links. Most likely this is because links are empty. Proceeding with empty links.");
+        }
         /** @var array<string, string> $entries */
-        $entries = unserialize($fs->get(self::LOCATION) ?: "") ?: [];
+        $entries = unserialize($contents) ?: [];
         return new Links($entries, self::LOCATION, $fs);
     }
 }
